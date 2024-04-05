bc779d2c078b214a9544add8eaa57d46547952ab
rayah <rayah@DESKTOP-B58KT0L>
2024-04-05 07:39:17 +0300

    Add project files.

---
+++ MauiApp7/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp7
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public List<StudentClass> StudentList = new List<StudentClass>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            StudentList.Add(new StudentClass(1, "Moe", "SE"));
+            StudentList.Add(new StudentClass(2, "Potato", "Hello World!"));
+            StudentList.Add(new StudentClass(3, "Panana", "AAA"));
+            StudentList.Add(new StudentClass(4, "hannah", "fxjdnm"));
+
+
+
+            Stu_List_View.ItemsSource = StudentList;
+
+        }
+
+        private async void Stu_List_View_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var student = e.Item as StudentClass;
+
+            await DisplayAlert("Selected student", "Student ID: " + student.ID.ToString(), "OK");
+        }
+
+        private void ADD_BUTT_Clicked(object sender, EventArgs e)
+        {
+            int ID;
+            int.TryParse(STU_ID.Text, out ID);
+            StudentList.Add(new StudentClass(ID, STU_NAME.Text, STU_DEPT.Text));
+            Stu_List_View.ItemsSource = null;
+            Stu_List_View.ItemsSource = StudentList;
+        }
+    }
+
+}
---
+++ MauiApp7/StudentClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp7
+{
+    public class StudentClass
+
+    {
+        public StudentClass(int ID, string Name, string Department)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Department = Department;
+
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Text1 { get; }
+        public string Text2 { get; }
+
+
+    }
+}
